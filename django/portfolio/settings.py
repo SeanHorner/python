85fa9676f722e514806b5974a5036360e37f9591
Sean Horner <sean.w.horner@gmail.com>
2021-09-01 13:21:25 -0500

    removed secret_key
    Key was an insecure, local-host only key, but removed for security's sake.

--- django/portfolio/settings.py
+++ django/portfolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jac!1)!_d19otv@-&v43vft%#qbz%_fqhhnl!b01lff&x(_e#u'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
